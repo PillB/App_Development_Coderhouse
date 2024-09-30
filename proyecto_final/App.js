8ab2a6c06fe7dd6e6c3ffe3780151d0c700b703f
Pillb <11358989+PillB@users.noreply.github.com>
2024-09-29 23:15:21 -0500

    Add Button and List

--- proyecto_final/App.js
+++ proyecto_final/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Proyecto Final!</Text>
       <StatusBar style="auto" />
+      <TextInput placeholder='Ingrese una tarea'/>
+      <Button title='Agregar Tarea'/>
     </View>
   );
 }
